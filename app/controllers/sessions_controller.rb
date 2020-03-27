10ac524ff71f7fcbc384231e6f5b349241eb9c56
gitnyasha <marshall.chikari@gmail.com>
2020-03-27 19:41:01 +0200

    created a user login session

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user
+      # Log the user in and redirect to the user's show page.
+      log_in user
+      redirect_to user
+    else
+      # Create an error message.
+      render "new"
+    end
+  end
+end
